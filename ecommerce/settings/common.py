977cd5f40731bd0f18d0b601816e36c6b9508882
Eaby Thomas C <eabythomascu@gmail.com>
2025-01-10 01:30:39 +0530

    render config

--- ecommerce/settings/common.py
+++ ecommerce/settings/common.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,11 +122,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR/'static',
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR/'static',
+# ]
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+DEBUG = 'RENDER' not in os.environ
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- ecommerce/settings/dev.py
+++ ecommerce/settings/dev.py
@@ -1,13 +1,17 @@
 from .common import *
 from dotenv import dotenv_values
+import os
 
 
-data=dotenv_values()
+data = dotenv_values()
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1','estorefront.store']
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "http://localhost:8000",]
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = data.get('SECRET_KEY')
@@ -20,7 +24,7 @@ DATABASES = {
         'HOST': data.get('DB_HOST'),
         'USER': data.get('DB_USER'),
         'PASSWORD': data.get('DB_PASSWORD'),
-        'port':data.get('DB_PORT')
+        'port': data.get('DB_PORT')
     }
 }
 
@@ -30,6 +34,6 @@ EMAIL_PORT = data.get('EMAIL_PORT')
 EMAIL_HOST_USER = data.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = data.get('EMAIL_HOST_PASSWORD')
 
-#razorpar
-RAZORPAY_ID=data.get('RAZORPAY_ID')
-RAZORPAY_ACCOUNT_ID=data.get('RAZORPAY_ACCOUNT_ID')
+# razorpar
+RAZORPAY_ID = data.get('RAZORPAY_ID')
+RAZORPAY_ACCOUNT_ID = data.get('RAZORPAY_ACCOUNT_ID')
