900e33ce2314d813e9cdf19c157378d17011a6cd
Eaby Thomas C <eabythomascu@gmail.com>
2025-01-10 09:50:16 +0530

    static n media

--- ecommerce/settings/common.py
+++ ecommerce/settings/common.py
@@ -127,6 +127,7 @@ STATIC_URL = '/static/'
 # ]
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'assets')
 DEBUG = 'RENDER' not in os.environ
 
 
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -16,27 +16,25 @@ Including another URLconf
 """
 import debug_toolbar
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
-    path('',include('core.urls')),
-    path('accounts/',include('allauth.urls')),
-    path('myadmin/',include('myadmin.urls')),
-    
+    path('', include('core.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('myadmin/', include('myadmin.urls')),
+
     # path('__debug__/', include(debug_toolbar.urls)),
-    
+
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
 
 
-
-######--Django Admin panel configuration here--#######
-admin.site.site_header="E-commerce Administration"
-admin.site.index_title="Admin Panel"
-
+###### --Django Admin panel configuration here--#######
+admin.site.site_header = "E-commerce Administration"
+admin.site.index_title = "Admin Panel"
