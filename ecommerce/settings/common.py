6be2c4df3ce157efbbd2356db8752a2e5c7ecfea
Eaby Thomas C <eabythomascu@gmail.com>
2025-01-10 10:20:12 +0530

    serverd debug1

--- ecommerce/settings/common.py
+++ ecommerce/settings/common.py
@@ -128,7 +128,8 @@ STATIC_URL = '/static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-DEBUG = 'RENDER' not in os.environ
+# DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
