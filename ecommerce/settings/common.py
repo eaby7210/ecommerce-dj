0db28700d1a3f204aaf8693ef40999fc2138a0e8
Eaby Thomas C <eabythomascu@gmail.com>
2025-01-10 09:52:31 +0530

    static root

--- ecommerce/settings/common.py
+++ ecommerce/settings/common.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 # ]
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 DEBUG = 'RENDER' not in os.environ
 
 
